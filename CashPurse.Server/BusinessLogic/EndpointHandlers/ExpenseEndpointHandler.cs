d91750415e0b6b2d6b0180c5c98382cdf020522c
Joseph Izang <josephizang@gmail.com>
2023-09-16 19:09:39 +0100

    fix gethandler

--- CashPurse.Server/BusinessLogic/EndpointHandlers/ExpenseEndpointHandler.cs
+++ CashPurse.Server/BusinessLogic/EndpointHandlers/ExpenseEndpointHandler.cs
@@ -16,15 +16,34 @@ public static class ExpenseEndpointHandler
     public static async Task<Ok<CursorPagedResult<List<ExpenseIndexModel>>>> HandleGet(ClaimsPrincipal principal,
         [FromServices] CashPurseDbContext context, [FromServices] UserManager<ApplicationUser> userManager)
     {
-        var userId = principal?.Identity?.Name!;
-        var user = await userManager.FindByNameAsync(userId).ConfigureAwait(false);
-        //if(request.Value == DateTimeOffset.MinValue || request is null)
-        //{
-        //    var result = await ExpenseDataService.GetUserExpenses(context, user!.Id).ConfigureAwait(false);
-        //    return TypedResults.Ok(new CursorPagedResult<IEnumerable<ExpenseIndexModel>>(result.Items[^1].ExpenseDate, result.Items));
-        //}
-        var request = DateTimeOffset.UtcNow;
-        var alternative = await ExpenseDataService.GetCursorPagedUserExpenses(context, user!.Id, request).ConfigureAwait(false);
-        return TypedResults.Ok(alternative!);
+        try
+        {
+            var userId = principal?.Identity?.Name!;
+            var user = await userManager.FindByNameAsync(userId).ConfigureAwait(false);
+            var cursor = DateTimeOffset.UtcNow;
+            var alternative = await ExpenseDataService.GetCursorPagedUserExpenses(context, user!.Id, cursor).ConfigureAwait(false);
+            return TypedResults.Ok(alternative!);
+        }
+        catch (Exception )
+        {
+            throw;
+        }
+    }
+
+    public static async Task<Ok<ExpenseIndexModel>> HandleGetById(ClaimsPrincipal principal,
+        [FromServices] CashPurseDbContext context, [FromServices] UserManager<ApplicationUser> userManager,
+        Guid expenseId)
+    {
+        try
+        {
+            var userId = principal?.Identity?.Name!;
+            var user = await userManager.FindByNameAsync(userId).ConfigureAwait(false);
+            var expense = await ExpenseDataService.GetExpenseById(context, user!.Id, expenseId).ConfigureAwait(false);
+            return TypedResults.Ok(expense!);
+        }
+        catch (Exception )
+        {
+            throw;
+        }
     }
 }
