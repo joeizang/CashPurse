4b411b215efba20a5bf3d76a3dc40a99a8dc6e3b
Joseph Izang <josephizang@gmail.com>
2023-09-25 13:46:21 +0100

    add packages and Expense Unit test class

---
+++ CashPurseServerTests/DataTests/ExpenseDataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using CashPurse.Server.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashPurseServerTests.DataTests
+{
+    public class ExpenseDataServiceTest
+    {
+        [Fact]
+        public async void CreateExpenseWithDataService_CreatesExpenseInDB()
+        {
+            //Arrange
+            var options = new DbContextOptions<CashPurseDbContext>();
+            await using var db = new CashPurseDbContext(options);
+
+            //Act
+
+
+            //Assert
+        }
+    }
+}
