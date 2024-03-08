354afcef8bfa15c07c9fc79740dbe6145814bf13
Joseph Izang <josephizang@gmail.com>
2024-03-08 16:08:04 +0100

    changes

--- CashPurse.Server/Program.cs
+++ CashPurse.Server/Program.cs
@@ -1,25 +1,12 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using CashPurse.Server;
-using CashPurse.Server.ApiModels;
-using CashPurse.Server.ApiModels.BudgetListApiModels;
-using CashPurse.Server.ApiModels.ExpensesApiModels;
-using CashPurse.Server.BusinessLogic.DataServices;
-using CashPurse.Server.BusinessLogic.Validators;
 using CashPurse.Server.Data;
 using CashPurse.Server.Endpoints;
 
 // using CashPurse.Server.Endpoints;
-using CashPurse.Server.Models;
-using CashPurse.Server.SwaggerConfig;
 using dotenv.net;
-using FluentValidation;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Swashbuckle.AspNetCore.SwaggerGen;
 using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
 DotEnv.Load();
@@ -81,23 +68,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 // app.MapGroup("/auth").MapIdentityApi<ApplicationUser>();
-app.MapGet("/api/health", async ([FromServices] CashPurseDbContext context,
-    IHttpClientFactory factory) =>
-{
-    var client = factory.CreateClient();
-    client.BaseAddress = new Uri("http://localhost:5086");
-    var budgetListsChecked = await context.BudgetLists.AsNoTracking().AnyAsync().ConfigureAwait(false);
-    var test = await context.Expenses.AsNoTracking().AnyAsync().ConfigureAwait(false);
-    var test1 = await client
-        .GetAsync("/api/budgetlists")
-        .ConfigureAwait(false);
-    var test2 = await client
-        .GetAsync("/api/expenses")
-        .ConfigureAwait(false);
-    if (budgetListsChecked && test && test1.IsSuccessStatusCode && test2.IsSuccessStatusCode)
-        return Results.Ok("API is up and running!");
-    return Results.Problem("API is up and running but there is a problem with the data!");
-});
 
 app.MapExpenseEndpoints();
 app.MapBudgetListEndpoints();
--- cashpurseclient/src/App.tsx
+++ cashpurseclient/src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button"
 
 function App() {
 
   return (
     <>
-      
+      <h1 className="font-bold text-3xl">Welcome to cash purse</h1>
+
+      <Button>Welcome to my app</Button>
     </>
   )
 }
